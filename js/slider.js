2f89d4f9dfad4e15f58c6b9abe663ee23baeba7c
kryukalexander <kryukalexander@gmail.com>
2016-03-16 13:44:18 +0600

    Закончен слайдер

--- js/slider.js
+++ js/slider.js
@@ -1,32 +1,90 @@
-/**
- * Created by user on 14.03.2016.
- */
-function ChangeSlideFromTo(val) {
-    $(".slide").fadeOut(1000);
-    $(".slide").eq(val).fadeIn(1000);
+function Toggle(from, to) {
+    if (from != to) {
+        Hide(from);
+        Show(to);
+    }
 }
 
-var x = $("input:checked").index("[type = radio]") + 1;
+function Show(i) {
+    var slide = $(".slide").eq(i);
+    var elem1 = slide.find(".el-1");
+    var elem2 = slide.find(".el-2");
+    var elem3 = slide.find(".el-3");
 
-$("[type = radio]").click(function () {
-        ChangeSlideFromTo($(this).index("[type = radio]"));
-        x = $(this).index("[type = radio]");
-        if (x != $("[type = radio]").length - 1) {
-            x++;
+    slide.animate({
+        opacity: "show"
+    }, 1000);
+    setTimeout(function() {
+        elem1.animate({
+            opacity: "show"
+        }, 1000);
+    }, 500);
+
+    setTimeout(function() {
+        elem2.animate({
+            opacity: "show"
+        }, 1000);
+    }, 1000);
+
+    setTimeout(function() {
+        elem3.animate({
+            opacity: "show"
+        }, 1000);
+    }, 1500);
+}
+
+function Hide(i) {
+    var slide = $(".slide").eq(i);
+    var elem1 = slide.find(".el-1");
+    var elem2 = slide.find(".el-2");
+    var elem3 = slide.find(".el-3");
+
+    slide.animate({
+        opacity: "hide"
+    }, 1000);
+    elem1.animate({
+        opacity: "hide"
+    }, 1000);
+    elem2.animate({
+        opacity: "hide"
+    }, 1000);
+    elem3.animate({
+        opacity: "hide"
+    }, 1000);
+}
+
+var controls = $("[name = slide_control]");
+var timer;
+var CurrentSlide = $("input:checked").index("[name = slide_control]");
+Show(CurrentSlide);
+
+controls.click(function() {
+    clearInterval(timer);
+    Toggle(CurrentSlide, $(this).index("[name = slide_control]"));
+    CurrentSlide = $(this).index("[name = slide_control]");
+    timer = setInterval(function() {
+        if (CurrentSlide + 1 == controls.length) {
+            Toggle(CurrentSlide, 0);
+            CurrentSlide = 0;
+        } else {
+            Toggle(CurrentSlide, CurrentSlide + 1);
+            CurrentSlide += 1;
         }
-        else {
-            x = 0
-        }
+        $("controls").eq(CurrentSlide).prop("checked", true);
+    }, 20000)
+
 });
 
-var cycle = setInterval(function () {
-    $("[type = radio]").eq(x).prop("checked", true);
-
-    ChangeSlideFromTo(x);
-    x++;
-
-    if (x > $("[type = radio]").length - 1) {
-
-        x = 0;
-    }
-}, 10000)
+/*Start timer when page loaded, with delay*/
+//setTimeout(function() {
+    timer = setInterval(function() {
+        if (CurrentSlide + 1 == $(controls).length) {
+            Toggle(CurrentSlide, 0);
+            CurrentSlide = 0;
+        } else {
+            Toggle(CurrentSlide, CurrentSlide + 1);
+            CurrentSlide += 1;
+        }
+        controls.eq(CurrentSlide).prop("checked", true);
+    }, 20000);
+//}, 5000)
